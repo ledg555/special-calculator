f95faee0ed07304b4424e9c0c41613bd46f89cf5
Luis Delgado <dev.ledg555@gmail.com>
2024-06-30 20:20:42 -0600

    Correct syntax error in solveSecondGradeEq function

--- mod-algebra/second-grade-eqs.js
+++ mod-algebra/second-grade-eqs.js
@@ -1,17 +1,17 @@
 function solveSecondGradeEq([a, b, c]) {
-  if (!a) {
-    if (!b) {
-      if (!c) return [Infinity]; // Means infinite solutions
-      else return [NaN]; // Means inconsistent equation
+  if (a === 0) {
+    if (b === 0) {
+      if (c === 0) return [Infinity]; // Infty, infinite solutions
+      else return [NaN]; // x !=== x, inconsistent equation
     }
-    else return [ -c / b ];
+    else return [-c / b]; // 1 element, 1 sol
   }
   else {
     let disc = b**2 - 4*a*c;
-    if (disc = 0) return [ -b / (2*a) ];
+    if (disc === 0) return [-b / (2 * a), -b / (2 * a)]; // 1 sol
     else if (disc > 0) return [ (-b + Math.sqrt(disc)) / (2*a),
-      (-b - Math.sqrt(disc)) / (2*a) ];
+      (-b - Math.sqrt(disc)) / (2*a) ]; // 2 sol
     else return [ -b / (2*a), Math.sqrt(-disc) / (2*a),
-      -Math.sqrt(-disc) / (2*a) ];
+      -Math.sqrt(-disc) / (2*a) ]; // 3 elem, complex sol
   }
 }
